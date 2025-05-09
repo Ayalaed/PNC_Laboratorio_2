cf0aeb33e25d47525b8fba6f52ff94417384dcd7
Ayalaed <00091722@uca.edu.sv>
2025-05-09 14:54:17 -0600

    Add:Every repositories :sparkles:

--- pnc_labo02/src/main/java/com/example/pnc_labo02/repository/DepartamentoRepository.java
+++ pnc_labo02/src/main/java/com/example/pnc_labo02/repository/DepartamentoRepository.java
@@ -1,6 +1,18 @@
 package com.example.pnc_labo02.repository;
 
+import com.example.pnc_labo02.model.Departamento;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
-public interface DepartamentoRepository {
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+
+    List<Departamento> findByNombre(String nombre);
+
+    @Query("SELECT d FROM Departamento d WHERE d.nombre LIKE %:nombre%")
+    List<Departamento> buscarPorNombre(String nombre);
+
+    @Query(value = "SELECT * FROM departamento WHERE nombre LIKE %?1%", nativeQuery = true)
+    List<Departamento> buscarNombreNativo(String nombre);
 }
