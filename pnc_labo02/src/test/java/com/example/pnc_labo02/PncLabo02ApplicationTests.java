521bb7bd26f791737fbe282855e36f1340c0458f
David <00004722@uca.edu.sv>
2025-05-08 20:48:57 -0600

    Init Commit

---
+++ pnc_labo02/src/test/java/com/example/pnc_labo02/PncLabo02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pnc_labo02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PncLabo02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
